14eb2d8634285ce6a47b1ca72e99654d126ccd7a
Arllan Felipe <arllansantiago@gmail.com>
2018-02-10 18:48:03 -0300

    Ajuste no EndPoint - categoria/{id}
    Criado tratamento de exceção para para categorias nao encontradas (not_found)

--- src/main/java/com/arllanfelipe/cursomc/services/CategoriaService.java
+++ src/main/java/com/arllanfelipe/cursomc/services/CategoriaService.java
@@ -2,8 +2,10 @@ package com.arllanfelipe.cursomc.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.arllanfelipe.cursomc.domain.Categoria;
 import com.arllanfelipe.cursomc.repositories.CategoriaRepository;
+import com.arllanfelipe.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -11,6 +13,11 @@ public class CategoriaService {
 	private CategoriaRepository repo;
 	public Categoria buscar(Integer id) {
 		Categoria obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado Id: " + id
+					+ " tipo: "+ Categoria.class.getName());
+		}
+		
 		return obj;
 	}
 }
