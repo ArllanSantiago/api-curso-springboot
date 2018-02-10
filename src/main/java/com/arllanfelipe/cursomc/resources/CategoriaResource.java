6f5b1ea862d169aa69909e34d3e34fe7a38ecfed
Arllan Felipe <arllansantiago@gmail.com>
2018-02-10 12:06:13 -0300

    Criando operação de Instaciação
    Como o banco e temporario os campos são criados apenas no momento de roda a aplicação.
    
    Foi criado o instanciamento automatico dos campos da tabela de categorias

--- src/main/java/com/arllanfelipe/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/arllanfelipe/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,11 @@
 package com.arllanfelipe.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.arllanfelipe.cursomc.domain.Categoria;
 import com.arllanfelipe.cursomc.services.CategoriaService;
 
